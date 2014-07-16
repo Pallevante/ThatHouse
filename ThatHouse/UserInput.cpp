0582e4d51da2cad2838de0762900bd1626f70555
Pallevante <pallevante@live.se>
2014-07-16 21:53:21 +0200

    Added implementation and comments for the class definition.

--- ThatHouse/UserInput.cpp
+++ ThatHouse/UserInput.cpp
@@ -1,6 +1,63 @@
 #include "UserInput.h"
-
+#include "Chapters.h"
+#include "Characters.h"
+#include <iostream>
+#include <algorithm>
+#include "Game.h"
 void UserInput::userInput()
 {
+	std::string input;
+	std::getline(std::cin, input);
+	allToLower(input);
 
+	if (contains(input, "translate"))
+	{
+		translate(input);
+	}
+	if (contains(input, "read"))
+	{
+
+	}
+	if (contains(input, "check"))
+	{
+		Game::mCurrentRoom->checkRoom();
+	}
+}
+
+void UserInput::allToLower(std::string& input)
+{
+	std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+}
+
+bool UserInput::contains(std::string& input, std::string phrase)
+{
+	std::size_t found = input.find(phrase);
+	if (found != std::string::npos)
+	{
+		return true;
+	}
+	else return false;
+}
+
+std::string UserInput::translate(std::string input)
+{
+	if (Characters::charachterFound(Characters::JUULI))
+	{
+		if (contains(input, "german"))
+		{
+
+		}
+		if (contains(input, "latin"))
+		{
+
+		}
+		if (contains(input, "spanish"))
+		{
+
+		}
+	}
+	else
+	{
+		return "The character that can translate theese isn't here.";
+	}
 }
--- ThatHouse/UserInput.h
+++ ThatHouse/UserInput.h
@@ -10,6 +10,18 @@ public:
 
 private:
 	static std::string  getResponse(std::string input);
+
+	// This will be triggered by writing translate. 
+	static std::string	translate(std::string input);
+
+	/// Used to convert all characters to lowercase.
+	/// This simplifies the usage of if's to compute the input
+	static void			allToLower(std::string& input);
+
+	// Used to find phrases in the input.
+	// ARGUMENTS: String to search, String what to look for.
+	static bool			contains(std::string& input, std::string phrase);
+
 	UserInput();
 	~UserInput();
 };
