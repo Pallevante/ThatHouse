b76e01096213446bb224b129fd98431529cfd32e
Pallevante <pallevante@live.se>
2014-07-22 17:40:18 +0200

    Changes in header-file: 	Added hit() and check(). Changes in source-file: 	UserINput loop. 	Implemented translation 	Implemented new methods. 	Loads of other shit...

--- ThatHouse/UserInput.cpp
+++ ThatHouse/UserInput.cpp
@@ -1,37 +1,51 @@
 #include "UserInput.h"
 #include "Chapters.h"
 #include "Characters.h"
-#include <iostream>
-#include <algorithm>
 #include "Game.h"
 #include "Note.h"
+#include "Player.h"
+#include "Rock.h"
+
+#include <iostream>
+#include <algorithm>
+
 
 Note* UserInput::mNote;
 ThreadWriting tw;
 
+
+// MAIN USER INPUT LOOP
 void UserInput::userInput()
 {
-	tw.wait();
-	std::string input;	
-	getline(cin, input);
-	allToLower(input);
-	if (contains(input, "inventory"))
+	bool isDoneWithPart = false;
+	while (!isDoneWithPart)
 	{
-		tw.write(Inventory::check());
+		tw.wait();
+		std::string input;
+		getline(cin, input);
+		allToLower(input);
+		if (contains(input, "inventory"))
+		{
+			tw.write(Inventory::check());
+		}
+		if (contains(input, "translate"))
+		{
+			tw.write(translate(input));
+		}
+		if (contains(input, "read"))
+		{
+			tw.write(read(input));
+		}
+		if (contains(input, "check"))
+		{
+			tw.write(check(input));
+		}
+		if (contains(input, "hit"))
+		{
+			tw.write(hit(input));
+		}
+		tw.wait();
 	}
-	if (contains(input, "translate"))
-	{
-		tw.write(translate(input));
-	}
-	if (contains(input, "read"))
-	{
-		tw.write(read(input));
-	}
-	if (contains(input, "check"))
-	{
-		tw.write(Game::mCurrentRoom->checkRoom());
-	}
-	tw.wait();
 }
 
 void UserInput::allToLower(std::string& input)
@@ -48,6 +62,43 @@ bool UserInput::contains(std::string& input, std::string phrase)
 	}
 	else return false;
 }
+
+std::string UserInput::check(std::string input)
+{
+	if (contains(input, "room"))
+	{
+		return Game::mCurrentRoom->checkRoom();
+	}
+
+	// Can only be done once
+	if (contains(input, "pillow"))
+	{
+		if (Player::isTiedToBed &&
+			Chapters::getChapter() == Chapters::CurrentChapter::FIRST
+			&& Chapters::getPart() == Chapters::CurrentPart::FIRST_PART)
+		{
+			Inventory::inventoryVector.push_back(new Rock());
+			return Inventory::inventoryVector[Inventory::inventoryVector.size() - 1]->getPickUpString();
+		}
+	}
+	else
+		return "Chech what?";
+}
+
+std::string UserInput::hit(std::string input)
+{
+	if (contains(input, "cuffs"))
+	{
+		if (Inventory::checkFor(Item::ROCK))
+		{
+			Player::isTiedToBed = false;
+			Chapters::progressPart();
+			return "After a while the cuffs break...";
+		}
+		else
+			return "With what dummy?";
+	}
+}		
 
 std::string UserInput::read(std::string input)
 {
@@ -104,26 +155,60 @@ std::string UserInput::read(std::string input)
 	}
 }
 
-
 std::string UserInput::translate(std::string input)
 {
 	if (Characters::charachterFound(Characters::JUULI))
 	{
 		if (contains(input, "german"))
 		{
-
+			for (auto i : Inventory::inventoryVector)
+			{
+				if (i->getType() == Item::NOTE)
+				{
+					mNote = mNote->returnNote(i);
+					if (mNote->getLang() == Note::GERMAN)
+					{
+						return mNote->translate();
+					}
+				}
+			}
+			return "You haven't found that note yet.";
 		}
+
 		if (contains(input, "swedish"))
 		{
-
+			for (auto i : Inventory::inventoryVector)
+			{
+				if (i->getType() == Item::NOTE)
+				{
+					mNote = mNote->returnNote(i);
+					if (mNote->getLang() == Note::SWEDISH)
+					{
+						return mNote->translate();
+					}
+				}
+			}
+			return "You haven't found that note yet.";
 		}
+
 		if (contains(input, "spanish"))
 		{
-
+			for (auto i : Inventory::inventoryVector)
+			{
+				if (i->getType() == Item::NOTE)
+				{
+					mNote = mNote->returnNote(i);
+					if (mNote->getLang() == Note::SPANISH)
+					{
+						return mNote->translate();
+					}
+				}
+			}
+			return "You haven't found that note yet.";
 		}
 		else
 		{
-			return "There isn't a letter in that language..";
+			return "What language?";
 		}
 	}
 	else
--- ThatHouse/UserInput.h
+++ ThatHouse/UserInput.h
@@ -7,6 +7,7 @@
 class Note;
 class UserInput
 {
+
 public:
 	static void			userInput();
 
@@ -15,7 +16,16 @@ private:
 
 	// This will be triggered by writing translate. 
 	static std::string	translate(std::string input);
+
+	// This can be used without the translation character.
 	static std::string  read(std::string input);
+	
+	// This is triggered by user input containing "check"
+	static std::string  check(std::string input);
+
+	// This is triggered by user input contaning "hit"
+	static std::string	hit(std::string input);
+
 	/// Used to convert all characters to lowercase.
 	/// This simplifies the usage of if's to compute the input
 	static void			allToLower(std::string& input);
