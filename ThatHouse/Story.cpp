e4f5a1f8e8176a02b0cd7f8ded76434250cb4ea7
Pallevante <pallevante@live.se>
2014-07-22 17:39:05 +0200

    Extended the getStory() method and added a getRoomType() method.

--- ThatHouse/Story.cpp
+++ ThatHouse/Story.cpp
@@ -1,12 +1,40 @@
 #include "Story.h"
+#include "Player.h"
+#include "Room.h"
+
+Room::RoomType Story::getRoomType()
+{
+	switch (Chapters::getChapter())
+	{
+	case Chapters::FIRST:
+		switch (Chapters::getPart())
+		{
+		case Chapters::FIRST_PART:
+			return Room::BEDROOM;
+
+		case Chapters::SECOND_PART:
+			return Room::BEDROOM;
+
+		case Chapters::THIRD_PART:
+			return Room::HALLWAY;
+		}
+	}
+}
 
 std::string Story::getStory()
 {
-	if (Chapters::getChapter() == Chapters::FIRST)
+	switch(Chapters::getChapter())
 	{
-		if (Chapters::getPart() == Chapters::FIRST_PART)
+	case Chapters::FIRST:
+		switch (Chapters::getPart())
 		{
+		case Chapters::FIRST_PART:
 			return "You find yourself... alone... in a strange room... \nYou try to move but you're handcuffed to a pipe in the wall.";
+		
+		case Chapters::SECOND_PART:
+			Player::isInDanger = true;
+			return "You get out of the bed and hear footsteps outside the door...";
+
 		}
 	}	
 }
